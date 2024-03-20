fb3942efdcac43e36e94c75291aaa2732aae9d51
josh klivan <jklivan@gmail.com>
2024-03-19 20:49:17 -0400

    Updated to try to run the fit function on causalANOVA

--- R_Causal.py
+++ R_Causal.py
@@ -9,13 +9,33 @@ utils = importr('utils')
 r('install.packages("FindIt", repos="https://CRAN.R-project.org/")')
 findit = importr('FindIt')
 
+'''messing around with how rpy2 works'''
 pi = robjects.r['pi']
 print(pi[0])
 rsum = robjects.r['sum']
 print(rsum(robjects.IntVector([1,2,3]))[0])
 
 
+'''using data from example in FindIt'''
 robjects.r('data("Carlson", package = "FindIt")')
 carlson = robjects.r['Carlson']
 carlson_df = pandas2ri.rpy2py(carlson)
-print(carlson_df)
+# print(carlson_df)
+
+'''trying a couple variables to use the 2 way amie'''
+new_Record = robjects.r('''
+Carlson$newRecordF<- factor(Carlson$newRecordF,ordered=TRUE,
+levels=c("YesLC", "YesDis","YesMP",
+"noLC","noDis","noMP","noBusi"))''')
+promise = robjects.r('''
+Carlson$promise <- factor(Carlson$promise,ordered=TRUE,levels=c("jobs","clinic","education"))
+''')
+model = robjects.r('''
+
+fit2 <- CausalANOVA(formula=won ~ newRecordF + promise,
+int2.formula = ~ newRecordF:promise,
+data=Carlson, pair.id=Carlson$contestresp,diff=TRUE,
+cluster=Carlson$respcodeS, nway=2)
+''')
+summary = robjects.r('''summary(fit2)''')
+print(summary)
