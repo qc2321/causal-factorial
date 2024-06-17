ffaaec6a26fab52f2e008bbb7ad4e6dbcce677d3
Quentin Chu <qc2321@columbia.edu>
2024-06-17 12:25:13 -0400

    Refactored evaluation methods into evaluate.py

---
+++ lasso_factorial_selection/evaluate.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.stats.weightstats import ztest
+from factorial_model import FactorialModel
+from forward_selection import ForwardSelection
+
+
+def sample_beta(fm, sampling_method=1, min_degree=2):
+    """Sample beta according to the beta_sampling_method"""
+    match sampling_method:
+        case 1:
+            return fm.sample_normal_beta()
+        case 2:
+            return fm.sample_skewed_beta(min_degree=min_degree)
+        case 3:
+            return fm.sample_interactive_beta_clusters(fm.k // 2)
+        case _:
+            raise ValueError(f"Invalid beta_sampling_method: {sampling_method}")
+        
+
+def evaluate_lasso(fm, seed=None):
+    """Evaluate Lasso model from FactorialModel instance"""
+    fm.sample_and_split_data(seed=seed)
+    fm.fit_lasso()
+    fm.predict()
+    fm.compute_mse()
+    fm.compute_r2()
+
+
+def evaluate_forward_selection(fs, T_test, y_test):
+    """Evaluate ForwardSelection model from given test data"""
+    fs.forward_selection()
+    fs.predict(T_test)
+    fs.compute_mse(y_test)
+    fs.compute_r2()
+
+
+def evaluate(fm, num_trials, seed=None, strong_heredity=False):
+    """Evaluate Lasso and ForwardSelection models"""
+    lasso_mses = []
+    fs_mses = []
+    lasso_r2s = []
+    fs_r2s = []
+    lasso_betas = []
+    fs_betas = []
+    lasso_expected_outcomes = []
+    fs_expected_outcomes = []
+
+    for i in range(num_trials):
+        evaluate_lasso(fm, seed=seed)
+        lasso_mses.append(fm.mse)
+        lasso_r2s.append(fm.r2)
+        lasso_betas.append(fm.beta_hat)
+        lasso_expected_outcomes.append(fm.expected_outcomes)
+
+        fs = ForwardSelection(fm.T_train, fm.y_train, fm.k, strong_heredity=strong_heredity)
+        evaluate_forward_selection(fs, fm.T_test, fm.y_test)
+        fs_mses.append(fs.mse)
+        fs_r2s.append(fs.r2)
+        fs_betas.append(fs.results.params)
+        beta_mask = fm.pf.fit_transform(fm.pf.powers_)
+        fs_expected_outcome = beta_mask @ fs.results.params
+        fs_expected_outcomes.append(fs_expected_outcome)
+
+    return lasso_mses, fs_mses, lasso_r2s, fs_r2s, lasso_betas, fs_betas, \
+           lasso_expected_outcomes, fs_expected_outcomes
+
--- lasso_factorial_selection/factorial_model.py
+++ lasso_factorial_selection/factorial_model.py
@@ -16,16 +16,19 @@ class FactorialModel:
         self.sparsity = sparsity
         self.contrast_coding = contrast_coding
         self.beta_seed = beta_seed
+
         # initialize interaction expansion transformation
         self.pf = preprocessing.PolynomialFeatures(
             degree=self.degree, interaction_only=True, include_bias=True
         )
         _ = self.pf.fit_transform(np.zeros((1, self.k), dtype="float64"))
         
+
     def sample_normal_beta(self):
         # initialize beta
         rng_beta = np.random.default_rng(self.beta_seed)
         self.beta = rng_beta.normal(0, 1, self.pf.n_output_features_).astype("float64")
+
         # impose sparsity on beta
         zero_indices = rng_beta.choice(
             self.pf.n_output_features_,
@@ -33,54 +36,67 @@ class FactorialModel:
             replace=False,
         )
         self.beta[zero_indices] = 0.0
+
         # normalize beta
         norm = np.linalg.norm(self.beta)
         self.beta = self.beta / norm
+
 
     def sample_skewed_beta(self, min_active_degree=1):
         # initialize beta
         rng_beta = np.random.default_rng(self.beta_seed)
         self.beta = rng_beta.normal(0, 1, self.pf.n_output_features_).astype("float64")
+
         # zero out coefficients for low order terms
         start = 1
         for i in range(1, min_active_degree):
             end = start + int(comb(self.k, i))
             self.beta[start:end] = 0.0
             start = end
+
         # normalize beta
         norm = np.linalg.norm(self.beta)
         self.beta = self.beta / norm
 
+
     def sample_interactive_beta_clusters(self, cluster_size):
         assert cluster_size <= self.k, "Cluster size must be less than or equal to number of factors (k)"
+        
         # initialize treatment clusters
         treatments = np.zeros((1, self.k), dtype="float64")
         treatments[:, :cluster_size] = 1
         cluster_mask = preprocessing.PolynomialFeatures(
             degree=self.k, interaction_only=True, include_bias=True
         ).fit_transform(treatments)
+       
         # initialize beta
         rng_beta = np.random.default_rng(self.beta_seed)
         self.beta = rng_beta.normal(0, 1, self.pf.n_output_features_).astype("float64")
         self.beta = self.beta * cluster_mask[0]
+        
         # normalize beta
         norm = np.linalg.norm(self.beta)
         self.beta = self.beta / norm
+
 
     def sample_and_split_data(self, test_size=0.2, seed=None):
         # sample treatment array
         rng = np.random.default_rng(seed)
         t = rng.binomial(1, self.p_t, (self.n, self.k)).astype("float64")
         t = t * 2 - 1 if self.contrast_coding else t
+
         # expand treatment array
         self.T = self.pf.fit_transform(t)
+
         # sample outcome with noise
         mu = self.T @ self.beta
         self.eps = rng.normal(0, self.sigma, size=self.n)
         self.y = mu + self.eps
+
         # split data
         list_of_splits = train_test_split(self.T, self.y, test_size=test_size, random_state=seed)
         self.T_train, self.T_test, self.y_train, self.y_test = list_of_splits
+
 
     def convert_and_split_data(self, t, y, is_dummy_coded=True, test_size=0.2, seed=None):
         if is_dummy_coded and self.contrast_coding:
@@ -89,6 +105,7 @@ class FactorialModel:
         list_of_splits = train_test_split(self.T, y, test_size=test_size, random_state=seed)
         self.T_train, self.T_test, self.y_train, self.y_test = list_of_splits
 
+
     def fit_lasso(self, logistic=False, alphas=None):
         if logistic:
             self.lasso = LogisticRegressionCV(fit_intercept=False, cv=5, penalty='l1',
@@ -96,18 +113,23 @@ class FactorialModel:
         else:
             self.lasso = LassoCV(alphas=alphas, fit_intercept=False, max_iter=10000, cv=5)
         self.lasso.fit(self.T_train, self.y_train)
+
         if logistic:
             self.beta_hat = self.lasso.coef_[0]
         else:
             self.beta_hat = self.lasso.coef_
+
         beta_mask = self.pf.fit_transform(self.pf.powers_)
         self.expected_outcomes = beta_mask @ self.beta_hat
+
 
     def predict(self):
         self.y_pred = self.lasso.predict(self.T_test)
     
+
     def compute_mse(self):
         self.mse = np.mean((self.y_pred - self.y_test) ** 2)
+
 
     def compute_r2(self):
         self.r2 = self.lasso.score(self.T_train, self.y_train)
